b17c7fdf3e3194c67a3b5039e180be4114d626c7
Adish Boodhun <adishpc321@gmail.com>
2025-08-21 10:48:14 +0400

    database keys

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,17 +97,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 if DEBUG:
-
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 else:
-    DATABASES={
-        "default": dj_database_url.parse(os.getenv("PROD_DATABASE_URL"))
+    prod_db_url = os.getenv("PROD_DATABASE_URL")
+    if not prod_db_url:
+        raise ValueError("PROD_DATABASE_URL environment variable not set!")
+    DATABASES = {
+        "default": dj_database_url.parse(prod_db_url)
     }
+
 
 
 # Password validation
